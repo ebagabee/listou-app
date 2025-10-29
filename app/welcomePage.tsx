dfe80c06bab5632233cd4e19e5eb737aeb92407d
ebagabee <ebagabe.2025@gmail.com>
2025-10-29 20:16:20 -0300

    feat: create home page

--- app/welcomePage.tsx
+++ app/welcomePage.tsx
@@ -1,13 +1,64 @@
-
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, Button, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import theme from "./theme"
+import WelcomeHero from "../assets/images/welcome-hero.png";
 
 export default function WelcomePage() {
     return (
-        <SafeAreaView>
-            <View>
-                <Text style={{ fontSize: 30, fontFamily: 'Nunito_400Regular' }}>Welcome Page</Text>
+        <SafeAreaView style={style.safeArea}>
+            <View style={style.container}>
+                <Text style={style.title}>
+                    Crie a sua{'\n'}<Text style={{ color: theme.colors.primary }}>Lista de Compras</Text>
+                </Text>
+
+                <Text style={style.subtitle}>
+                    Com o Listou, você pode criar{'\n'}
+                    e organizar suas compras com{'\n'}
+                    facilidade
+                </Text>
             </View>
+            <Image source={WelcomeHero} style={style.heroImage}></Image>
+
+            <TouchableOpacity style={style.custonButton}>
+                <Text style={{ color: "#fff", fontSize: 20, textAlign: 'center', fontFamily: 'Nunito_700Bold' }}>
+                    Criar minha lista!
+                </Text>
+            </TouchableOpacity>
         </SafeAreaView>
     );
 }
+
+const style = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    container: {
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    title: {
+        fontSize: 32,
+        fontFamily: 'Nunito_700Bold',
+        textAlign: 'center',
+        marginBottom: 24,
+    },
+    subtitle: {
+        fontSize: 20,
+        fontFamily: 'Nunito_400Regular',
+        textAlign: 'center',
+        color: '#555',
+    },
+    heroImage: {
+        width: 350,
+        objectFit: "contain"
+    },
+    custonButton: {
+        padding: 14,
+        backgroundColor: '#FF5900',
+        borderRadius: 40,
+        width: 300
+    }
+});
