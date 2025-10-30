64567e0c4e68b90e130c4b0887a5f29636b8a828
ebagabee <ebagabe.2025@gmail.com>
2025-10-30 19:36:59 -0300

    feat: adiciona header

--- components/defaultHeader.tsx
+++ components/defaultHeader.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Settings2 } from "lucide-react-native";
 
 export default function DefaultHeader() {
     return (
         <SafeAreaView>
-            <View>
-                <Text>Header Personalizado aqui (Isso foi o gabe que escreveu nao ia)</Text>
+            <View style={{ padding: 10, paddingHorizontal: 16, display: 'flex', flexDirection: 'row', justifyContent: "space-between" }}>
+                <Text style={{ fontSize: 20, fontStyle: "italic", fontWeight: 800 }}>Listou</Text>
+                <Settings2 size={26} />
             </View>
         </SafeAreaView>
     )
