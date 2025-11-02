34da008bdd4c86c4feb363f52f2c2e3cf7604ce7
ebagabee <ebagabe.2025@gmail.com>
2025-11-02 13:21:47 -0300

    feat: adiciona icones de lapis e lixeira

--- components/shopping/ItemList.tsx
+++ components/shopping/ItemList.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { Checkbox } from 'expo-checkbox';
 import { useState } from "react";
 import theme from "../../app/theme";
+import { Pencil, Trash } from "lucide-react-native";
 
 interface ItemListProps {
     name: string,
@@ -12,7 +13,8 @@ export default function ItemList({ name }: ItemListProps) {
 
     return (
         <View style={styles.container}>
-            <Checkbox
+            <View style={styles.leftContainer}>
+                <Checkbox
                 value={isChecked}
                 onValueChange={setChecked}
                 style={styles.checkbox}
@@ -24,11 +26,12 @@ export default function ItemList({ name }: ItemListProps) {
                     <Text style={styles.info}> Qtd: 4 | R$ 40,00 </Text>
                 </View>
             </View>
+            </View>
 
             {/* TODO: Adicionar os botoes aqui */}
             <View style={styles.actionBtns}>
-                <Text>teste </Text>
-                <Text>teste </Text>
+                <Pencil color={theme.colors.text2}/> 
+                <Trash color={theme.colors.negative}/>
             </View>
         </View>
     );
@@ -40,11 +43,17 @@ const styles = StyleSheet.create({
         width: '100%',
         flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    leftContainer: {
+        flexDirection: 'row',
         gap: 5,
+        alignItems: 'center',
     },
     name: {
         fontSize: 16,
         fontWeight: 400,
+        marginLeft: 2,
     },
     info: {
         fontSize: 12,
