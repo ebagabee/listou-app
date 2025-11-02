9c8714863085f554d274a9487d28c4982fa34aec
ebagabee <ebagabe.2025@gmail.com>
2025-11-02 17:30:18 -0300

    feat: adiciona funcionalidade de back na arrow do header

--- components/defaultHeader.tsx
+++ components/defaultHeader.tsx
@@ -2,6 +2,7 @@ import { Text, View, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ArrowLeft, Settings2 } from "lucide-react-native";
 import theme from "../app/theme";
+import { useNavigation } from "expo-router";
 
 interface propsInterface  {
     title?: string;
@@ -10,11 +11,13 @@ interface propsInterface  {
 }
 
 export default function DefaultHeader({title, back, settings = true}: propsInterface) {
+    const navigation = useNavigation();
+
      return (
         <SafeAreaView style={styles.safeArea} edges={['top']}>
             <View style={styles.headerContainer}>
                 <View style={{flexDirection: 'row', gap: 4}}>
-                    {back ? <ArrowLeft /> : null}
+                    {back ? <ArrowLeft onPress={navigation.goBack}/> : null}
                     <Text style={styles.logoText}>{title ? title : 'L i s t o u'}</Text>
                 </View>
                 {settings ? <Settings2 size={26} color={theme.colors.text}/> : null }
